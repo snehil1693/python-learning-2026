05f14a6e572dadba5e779432e39776dc9c088704
Snehil Shubham <snehil1693@gmail.com>
2026-04-01 13:48:54 +0530

    third

--- First.py
+++ First.py
@@ -12,6 +12,25 @@ print("I am learning Python")
 
 print("Hello")
 
+print("Hello", "World")
+
+print(10)
+
+print(20)
+
+print(10 + 20)
+
+print(5 + 2)
+print(10 - 5)
+print(5 * 2)
+print(10 / 2)
+
+print("Snehil")
+print("8 years")
+print(10 + 20)
+print("I am learning DSA")
+
+
 
 
 

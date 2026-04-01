2f185137d221a1036b846886c1cf36eead853ac9
Snehil Shubham <snehil1693@gmail.com>
2026-04-01 14:10:01 +0530

    fourth

---
+++ second.py
@@ -0,0 +1,30 @@
+
+x = 10
+name = "Snehil"
+experience = 8
+
+print(name , x)
+print(name)
+print(x)
+print(experience)
+
+a = 10
+b = 20
+
+c = a + b
+
+print(c)
+
+new_name = "Snehil Shubham"
+print(new_name)
+
+experience = 9
+
+print(experience)
+
+d = 90
+e = 10
+
+f = d + e
+
+print(f)

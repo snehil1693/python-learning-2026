544a182790ca04850f6dc47a9999bdf64e33e7d7
Snehil Shubham <snehil1693@gmail.com>
2026-04-08 12:05:05 +0530

    commit 5

---
+++ third.py
@@ -0,0 +1,23 @@
+
+name = input("Enter your name: ")
+print("Hello", name)
+
+name = input("Enter your name: ")
+print(name)
+
+age = input("Enter your age: ")
+print(age)
+
+age_1 = input("Enter age: ")
+print(age_1)
+
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+print("The sum is:", a + b)
+
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+print("The sum is:", a + b)
+

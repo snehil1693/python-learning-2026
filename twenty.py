3f930e1c0e153c1bbd8722a197bce2d725fb7e41
Snehil Shubham <snehil1693@gmail.com>
2026-04-10 14:02:16 +0530

    thirteen

---
+++ twenty.py
@@ -0,0 +1,9 @@
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print(a, "first number is greater than second number", b)
+else:
+    print(b, "second number is greater than first number", a)
---
+++ twentyone.py
@@ -0,0 +1,8 @@
+
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult and eligible to vote.")
+else:
+    print("You are a minor and not eligible to vote.")

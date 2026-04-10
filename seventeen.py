8038b51247187d467036c4e003077a85024b6d45
Snehil Shubham <snehil1693@gmail.com>
2026-04-10 13:48:15 +0530

    eleven

---
+++ seventeen.py
@@ -0,0 +1,9 @@
+
+
+p = int(input("Enter the principal amount: "))
+r = int(input("Enter the rate of interest: "))
+t = int(input("Enter the time in years: "))
+
+si = (p * r * t) / 100
+
+print("The simple interest is:", si)

ca0d34a19e00f7905737dad90088b30a26b575ee
Snehil Shubham <snehil1693@gmail.com>
2026-04-14 16:30:50 +0530

    sixteen

---
+++ fourtyfive.py
@@ -0,0 +1,9 @@
+
+num = int(input("Enter a number: "))
+
+for i in range(1, num + 1):
+    for j in range(1, i+ 1):
+        print(i, end=" ")
+    print()
+
+print("Pattern printed.")
---
+++ fourtythree.py
@@ -0,0 +1,9 @@
+
+num = int(input("Enter a number: "))
+
+for i in range(num, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
+
+print("Pattern printed.")
